5f7f5054ddc8a93c4ff9437a77b7bf029481c540
Asus <newg91945@gmail.com>
2026-03-03 22:21:40 +0530

    Stage 3 : Database and models

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+database_url = "sqlite:///./test.db"
+
+engine = create_engine(database_url, connect_args={"check_same_thread":False})
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
